2e2d3417cfcf817599c704053dfaf3acc19b8b6f
Tara025 <tara_jones@web.de>
2023-05-29 12:47:52 +0200

    Artikelbeschreibung

--- src/components/Articel/Articel.jsx
+++ src/components/Articel/Articel.jsx
@@ -4,6 +4,8 @@ import Einfachs from "../../images/Einfachs.jpg";
 function Articel() {
   return (
     <div className={styles.articel}>
+      <h4>Brauerei Püttner aus Schlammersdorf!</h4>
+      <h5>Einfachs/Hell</h5>
         
         <img src={Einfachs} alt="beer" />
     </div>
